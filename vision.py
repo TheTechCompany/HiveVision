71f8d174f2990d08b1aafd06025ac0b7fe455b1a
Ross Leitch <professional.balbatross@gmail.com>
2021-11-27 18:19:09 +1300

    Init

---
+++ vision.py
@@ -0,0 +1,22 @@
+import torch
+import cv2
+
+print("Loading YOLO...")
+model = torch.hub.load('ultralytics/yolov3', 'yolov3')
+print("Loaded model")
+vid = cv2.VideoCapture(0)
+
+while(True):
+
+    ret, frame = vid.read()
+
+    results = model(frame)
+
+    results.print()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+
+

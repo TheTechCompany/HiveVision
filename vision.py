603085ed49917329721c0a96faf320ca7f400ba8
Ross Leitch <professional.balbatross@gmail.com>
2021-12-18 21:19:00 +1300

    Reload

--- vision.py
+++ vision.py
@@ -1,8 +1,13 @@
+from typing import Tuple
 import torch
 import cv2
+import requests
+import time
+
+telemetry = "http://192.168.255.62:4200/api/telemetry"
 
 print("Loading YOLO...")
-model = torch.hub.load('ultralytics/yolov3', 'yolov3')
+model = torch.hub.load('ultralytics/yolov3', 'yolov3', force_reload=True)
 print("Loaded model")
 vid = cv2.VideoCapture(0)
 
@@ -12,9 +17,14 @@ while(True):
 
     results = model(frame)
     
-    cv2.imshow('frame', frame)
+    print(results)
 
-    results.print()
+    #event, properties, source, timestamp 
+    #x = requests.post(
+    #    telemetry, 
+    #    data={ 'event': 'camera-yolo', 'properties': results, 'source': 'camera', 'timestamp': time.time() })
+
+    time.sleep(5)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
